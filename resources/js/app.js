d9c5ad4ab4058aa27a2892d805335a0bd1421925
siryu-saito <saito.s.ryu@gmail.com>
2021-10-25 14:25:34 +0900

    ビューの作成

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,41 @@
 require('./bootstrap');
 
+// HTMLの読み込みが終わった後に実行される処理を設定
 window.addEventListener('DOMContentLoaded', function(event) {
-  let deleteButtons = document.querySelectorAll('.delete-quiz');
+    // delete-quiz の class があたっているタグ(ボタン)をすべて取得(配列)
+    let deleteButtons = document.querySelectorAll('.delete-quiz');
 
-  deleteButtons.forEach(deleteButton => {
-    deleteButton.addEventListener('click', function (event) {
-      let quizId = this.dataset.id;
+    // ボタンを一つずつ処理する
+    deleteButtons.forEach(deleteButton => {
+        // ボタンにクリックイベントを追加
+        deleteButton.addEventListener('click', function (event) {
+            let quizId = this.dataset.id;
 
-      if (!confirm('id:' + quizId + ' のQuizを削除します\nよろしいですか？')) {
-        return;
-      }
+            // 削除前の確認を表示
+            if (!confirm('id:' + quizId + ' のQuizを削除します\nよろしいですか?')) {
+                return;
+            }
 
-      axios.delete('/quizzes' + quizId)
-        .then(function (response) {
-          let row = event.target.closest("tr");
-          row.parentNode.removeChild(row);
+            // ボタンのタグについている属性data-idに設定されている値(data-idの値)をURLにしてサーバーにリクエスト
+            // リクエストメソッドが DELETE で送信
+            // axios https://github.com/axios/axios
+            axios.delete('/quizzes/' + quizId)
+                .then(function (response) {
+                    // リクエストが正常に処理された場合
 
-          alert('削除しました')
-          console.log(response);
-        })
+                    // テーブルから消す
+                    let row = event.target.closest("tr");
+                    row.parentNode.removeChild(row);
 
-        .catch(function (error) {
-          alert(error)
-          console.log(error);
+                    // メッセージ表示
+                    alert('削除しました')
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // リクエストで何らかのエラーが発生した場合
+                    alert(error)
+                    console.log(error);
+                });
         });
     });
-  });
 });
