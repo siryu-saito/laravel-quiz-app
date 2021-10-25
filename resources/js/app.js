2afdbba084502d4d48ec7e152638d659db4d837a
siryu-saito <saito.s.ryu@gmail.com>
2021-10-25 11:28:46 +0900

    テンプレートの実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
 require('./bootstrap');
+
+window.addEventListener('DOMContentLoaded', function(event) {
+  let deleteButtons = document.querySelectorAll('.delete-quiz');
+
+  deleteButtons.forEach(deleteButton => {
+    deleteButton.addEventListener('click', function (event) {
+      let quizId = this.dataset.id;
+
+      if (!confirm('id:' + quizId + ' のQuizを削除します\nよろしいですか？')) {
+        return;
+      }
+
+      axios.delete('/quizzes' + quizId)
+        .then(function (response) {
+          let row = event.target.closest("tr");
+          row.parentNode.removeChild(row);
+
+          alert('削除しました')
+          console.log(response);
+        })
+
+        .catch(function (error) {
+          alert(error)
+          console.log(error);
+        });
+    });
+  });
+});
